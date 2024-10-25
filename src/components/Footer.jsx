0743febe62f350e66df149bcb5d4524648419722
Adelya <msvadelya@gmail.com>
2024-10-25 18:51:11 +0200

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer class="bg-purple-400 font-sans dark:bg-gray-900">
       <div class="container px-6 py-12 mx-auto">
-        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
+        <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-3">
           <div class="sm:col-span-2">
             <h1 class="max-w-lg text-xl font-semibold tracking-tight text-gray-800 xl:text-2xl dark:text-white">
               Subscribe our newsletter to get an update.
@@ -16,45 +16,9 @@ function Footer() {
                 placeholder="Email Address"
               />
 
-              <button class="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
+              <button class="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-secondary rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
                 Subscribe
               </button>
-            </div>
-          </div>
-
-          <div>
-            <p class="font-semibold text-gray-800 dark:text-white">
-              Quick Link
-            </p>
-
-            <div class="flex flex-col items-start mt-5 space-y-2">
-              <p class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">
-                Home
-              </p>
-              <p class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">
-                Who We Are
-              </p>
-              <p class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">
-                Our Philosophy
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <p class="font-semibold text-gray-800 dark:text-white">
-              Industries
-            </p>
-
-            <div class="flex flex-col items-start mt-5 space-y-2">
-              <p class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">
-                Retail & E-Commerce
-              </p>
-              <p class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">
-                Information Technology
-              </p>
-              <p class="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">
-                Finance & Insurance
-              </p>
             </div>
           </div>
         </div>
@@ -65,12 +29,6 @@ function Footer() {
           <div class="flex flex-1 gap-4 hover:cursor-pointer">
             <img
               src="https://www.svgrepo.com/show/303139/google-play-badge-logo.svg"
-              width="130"
-              height="110"
-              alt=""
-            />
-            <img
-              src="https://www.svgrepo.com/show/303128/download-on-the-app-store-apple-logo.svg"
               width="130"
               height="110"
               alt=""
@@ -125,7 +83,7 @@ function Footer() {
           </div>
         </div>
         <p class="font-sans p-8 text-start md:text-center md:text-lg md:p-4">
-          © 2023 You Company Inc. All rights reserved.
+          © 2024 Lange Nacht des Coding. All rights reserved.
         </p>
       </div>
     </footer>
