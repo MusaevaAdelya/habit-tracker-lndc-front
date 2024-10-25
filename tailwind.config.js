75b002da393dea70b5d936b22cbca5fcb83ee736
Adelya <msvadelya@gmail.com>
2024-10-25 13:53:44 +0200

    added fonts and color varibales

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ['"Sulphur Point"', "sans-serif"],
+        secondary: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#9B5DE5",
+        secondary: "#e22d93",
+        dark: {
+          100: "#212529",
+        },
+        light: {
+          100: "#fdfcf7",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
